5b2892693f158f0fded98d9edf3ce5da3eb9569f
Adilfarooque <Adilfarq11@gmail.com>
2024-04-17 15:41:41 +0530

    Updated

---
+++ pkg/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port          string `mapstructure:"PORT"`
+	DBHost        string `mapstructure:"DBHost"`
+	DBPort        string `mapstructure:"DBPort"`
+	DBUser        string `mapstructure:"DBUser"`
+	DbPassword    string `mapstructure:"DBPassword"`
+	DBName        string `mapstructure:"DBName"`
+	ProductSvcUrl string `mapstructure:"PRODUCT_SVC_URL"`
+}
+
+func LoadConfig() (confg Config, err error) {
+	viper.AddConfigPath("./pkg/config/envs")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.SetDefault("PORT", ":50053")
+	viper.SetDefault("DBHost", "localhost")
+	viper.SetDefault("DBPort", "5432")
+	viper.SetDefault("DBUser", "postgres")
+	viper.SetDefault("DBPassword", "7356")
+	viper.SetDefault("DBName", "order_svc")
+	viper.SetDefault("PRODUCT_SVC_URL", "localhost:50052")
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&confg)
+
+	return
+}
---
+++ pkg/db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Adilfarooque/microServices-grpc-project_order_svc/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+
+func Init(dbHost,dbPort,dbUser,dbPassword,dbName string)Handler{
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",dbHost,dbPort,dbUser,dbPassword,dbName)
+
+	db , err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(models.Order{})
+
+	return Handler{db}
+}
